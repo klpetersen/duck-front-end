514351a277ec00c9856c177ffbd31d59e1d15ac0
github username <github email address>
2019-11-11 14:02:15 -0500

    components organized

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import LeaderBoard from './components/LeaderBoard'
+import Game from './components/Game'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+render(){
+    return (
+      <Router>
+        <div>
+          <NavBar /> 
+          <Route path="/" render={() => <Home /> }/>
+          <Route path="/login" render={() => <Login /> } /> 
+          <Route path='/signup' render={() => <SignUp /> } /> 
+          <Route path='/leaderboard' render={() => <LeaderBoard /> } /> 
+          <Route path='/game' render={() => <Game /> } /> 
+        </div>
+      </ Router>
+    
+    )
+  }
 }
 
 export default App;
