e4550fba5b676350d5ca8e4c29daeb513ca902da
github username <github email address>
2019-11-13 15:15:17 -0500

    timer is added

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,26 +4,38 @@ import Timer from './Timer';
 
 export default class Game extends Component {
     state = {
-        num: 1
+        num: 1, 
+        seconds: 45, 
+        gameOver: false
     }
 
     componentDidMount(){
         let navbar = document.getElementsByClassName('navigation-bar show')[0];
         navbar.classList.add('hidden');
         navbar.classList.remove('show');
+        this.updateTimer();
     }
 
-    // addNum = (number) => {
-    //     this.setState({
-    //         num: number
-    //     })
-    //     console.log(number)
-    // }
+    updateTimer = () => { 
+        setInterval(() => { 
+            if (this.state.seconds > 0) { 
+                this.setState({ 
+                    seconds: this.state.seconds - 1
+                }) 
+            } else if (this.state.seconds === 0){ 
+                this.setState({ 
+                    gameOver: true
+                })
+            }
+        }, 1000 ) 
+       
+    }
+ 
 
     render() {
         return (
             <div>
-                <Timer />
+                <Timer seconds={this.state.seconds} />
                 <Canvas num={this.state.num} addNum={this.addNum}/>
             </div>
         )
