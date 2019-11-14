7a632117f7ce237af7cd492536985eb1bac2f6f8
github username <github email address>
2019-11-14 15:52:33 -0500

    fix with seb help commented out

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,40 +5,67 @@ import duckie from '../ducky.png';
 
 export default class Home extends Component {
 
-
-    // componentDidMount(){
-    //     const history = document.querySelector('.history-list');
-    //     let list = this.fetchGames()
-    //     console.log(list);
-    //     history.innerHTML = list;
-    // }
-
-    fetchGames = () => {
-        fetch('http://localhost:3000/games')
-            .then(resp => resp.json())
-            .then(data => this.findUserGames(data))
-    }
-
-    findUserGames = (games) => { 
-        let foundGames = games.filter(game => game.user_id === this.props.currentUser.id)
-        // console.log(foundGames)
-        return foundGames.map(game=> <li>{game.created_at}:{game.score}</li>)
-    }
     
-    render() {
-        if(this.props.currentUser){
-            return (
-                <div>
-                    <h1>welcome, {this.props.currentUser.name}</h1>
-                    <ul className="history-list"></ul>
-                </div>
-            )
-        }else{
-            return (
-                <div id="viewer">
-                    <img id="duckie" src={duckie} alt="duckie"/>
-                </div>
-            )
-        }
-    }
 }
+
+
+// state = {
+//     // userGames: [],
+//     allGames: [],
+//     currentUser: null
+// }
+
+// static getDerivedStateFromProps(props, state) {
+//     if (props.currentUser !== state.currentUser) {
+//         return {
+//           currentUser: props.currentUser,
+//         };
+//       }
+  
+//       // Return null if the state hasn't changed
+//       return null;
+// }
+
+// componentDidMount() {
+//     // fetchGames = () => {
+//         fetch('http://localhost:3000/games')
+//             .then(resp => resp.json())
+//             // .then(data => this.findUserGames(data))
+//             .then(data => this.setState({
+//                 allGames: data
+//             }))
+//             // .then(data => this.findUserGames())
+//     // }
+// }
+
+// findUserGames = () => { 
+//     let foundGames = this.state.allGames.filter(game => game.user_id === this.state.currentUser.id)
+//     return foundGames
+// }
+
+// renderGames = () => {
+//     let userGames = this.findUserGames()
+//     console.log(userGames)
+//     return userGames.map(game => <Show score={game.score} time={game.created_at}/>)
+//     // console.log(list)
+//     // return list;
+// }
+
+// render() {
+//     console.log(this.state)
+//     if(this.state.currentUser){
+//         return (
+//             <div>
+//                 <h1>welcome, { this.state.currentUser ? this.state.currentUser.name : null }</h1>
+//                 {/* {this.fetchGames()} */}
+//                 { this.renderGames() }
+//             </div>
+//         )
+//     }else{
+//         return (
+//             <div id="viewer">
+//                 <img id="duckie" src={duckie} alt="duckie"/>
+//             </div>
+//         )
+//     }
+// }
